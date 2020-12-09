b65c22ead70a9a4e62819683fcb09fd8e1e2d9ba
roshni-patel <rpatel01@wesleyan.edu>
2020-12-09 10:24:12 -0600

    Panagram function and tests complete

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,24 @@
-const isPangram = function(text) {
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
 
+const isPangram = function (text) {
+    const letterCount = {};
+
+    const letters = text.split('');
+    letters.forEach((letter) => {
+        if (letterCount[letter]) {
+            letterCount[letter] += 1;
+    }   else {
+            letterCount[letter] = 1; 
+    }
+});
+
+for (const letter of ALPHABET) {
+    if (!letterCount[letter]) {
+        return false; 
+    }
+}
+
+    return true;
 };
 
 module.exports = isPangram;
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -10,54 +10,78 @@ describe('isPangram()', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
+    const text = 'abcdefghijklmnopqrstuvwxyz';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(true);
 
   });
 
   test("missing character 'x'", () => {
     // Arrange
+    const text = 'the quick brown fo jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toEqual(false);
   });
 
   test('empty sentence', () => {
     // Arrange
+    const text = '';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(false);
 
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toEqual(true);
   });
 
   test('pangram with numbers', () => {
     // Arrange
+    const text = 'the quick brown fox jumps 19 feet over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toEqual(true);
   });
 
   // Write your own test case
+  test('returns true for text with each letter multiple times', () => {
+    // Arrange
+    const text = 'abcdefghijklmnopqrstuvwxyz' + 'abcdefghijklmnopqrstuvwxyz';
+
+    // Act
+    const answer = isPangram(text);
+
+    // Assert 
+    expect(answer).toEqual(true);
+  });
 });
